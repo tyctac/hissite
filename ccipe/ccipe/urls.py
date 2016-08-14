91dee831f18bbaa3203599946c539edc8cab6cb6
tyctac <2283499971@qq.com>
2016-08-14 21:06:46 +0800

    second that works

--- ccipe/ccipe/urls.py
+++ ccipe/ccipe/urls.py
@@ -17,13 +17,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import settings
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^home/',include('home.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 urlpatterns += staticfiles_urlpatterns()
